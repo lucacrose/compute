cef0d644ecd12be1f82a4beecff69a4e741a9c4e
Luca Rose <sceptive@gmail.com>
2026-09-11 06:57:13 -0500

    Command line arguements support

--- main.cpp
+++ main.cpp
@@ -2,22 +2,26 @@
 #include <random>
 #include <cstddef>
 #include <vector>
+#include <string>
 #include "values.hpp"
 #include "baskets.hpp"
 
-constexpr std::size_t items_count = 100;
-constexpr std::size_t time_steps  = 2048;
-constexpr std::size_t baskets_count = 4;
+int main(int argc, char* argv[]) {
+    if (argc < 4) {
+        std::cerr << "Usage: " << argv[0] << " <items_count> <time_steps> <baskets_count>\n";
+        return 1;
+    }
 
-int main() {
+    std::size_t items_count = std::stoull(argv[1]);
+    std::size_t time_steps = std::stoull(argv[2]);
+    std::size_t baskets_count = std::stoull(argv[3]);
+
     std::random_device rd;
     std::mt19937 gen(rd());
 
     std::vector<backtester::ItemResult> item_values = backtester::generate_values(items_count, time_steps, gen);
 
     std::vector<backtester::Basket> baskets = backtester::generate_baskets(baskets_count, gen);
-
-    std::cout << item_values.size() << std::endl;
 
     return 0;
 }
